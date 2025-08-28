a15ee8d11efe29e2a3dc5ff37f4f704b3271f268
Michael Stahnke <stahnke@floxdev.com>
2025-08-28 16:56:00 -0500

    feat: Remove threading

--- slack/slack.go
+++ slack/slack.go
@@ -23,31 +23,22 @@ func NewService(botToken, channelID string) *Service {
 	}
 }
 
-// PostTrackInfo posts the track info with artist and song name, then adds links in a threaded comment
+// PostTrackInfo posts the track info with artist, song name, and both Spotify and YouTube links
+// in a single message. Spotify links are clickable but don't unfurl, YouTube search links are formatted clearly.
 func (s *Service) PostTrackInfo(artist, songName, spotifyURL string) error {
-	// Post the main message with just artist and song name
-	message := fmt.Sprintf("%s - %s", artist, songName)
-
-	_, timestamp, err := s.client.PostMessage(
-		s.channelID,
-		slack.MsgOptionText(message, false),
-	)
-	if err != nil {
-		return err
-	}
-
 	// Create YouTube search URL
 	searchQuery := fmt.Sprintf("%s %s", artist, songName)
 	youtubeURL := fmt.Sprintf("https://www.youtube.com/results?search_query=%s",
 		url.QueryEscape(searchQuery))
 
-	// Post links as a threaded reply
-	linksMessage := fmt.Sprintf("Spotify: %s\nYouTube: %s", spotifyURL, youtubeURL)
+	// Post the main message with artist, song name, and both links
+	// Disable unfurling to prevent URL previews, but links remain clickable
+	message := fmt.Sprintf("%s - %s\n🎵 Spotify: %s\n🔍 YouTube: %s", artist, songName, spotifyURL, youtubeURL)
 
-	_, _, err = s.client.PostMessage(
+	_, _, err := s.client.PostMessage(
 		s.channelID,
-		slack.MsgOptionText(linksMessage, false),
-		slack.MsgOptionTS(timestamp), // This makes it a threaded reply
+		slack.MsgOptionText(message, false),
+		slack.MsgOptionDisableLinkUnfurl(), // Prevent URL previews but links remain clickable
 	)
 
 	return err
